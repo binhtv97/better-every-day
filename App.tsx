9a89bbca019782518654cf54efe983502119a582
binhtran.KMS <88226711+binhtv97@users.noreply.github.com>
2025-08-08 01:48:58 +0700

    fix: update

--- App.tsx
+++ App.tsx
@@ -1,1 +1,10 @@
-import 'expo-router/entry';
+import { registerRootComponent } from 'expo';
+import { ExpoRoot } from 'expo-router';
+
+// Export the root component
+export default function App() {
+  const ctx = require.context('./app');
+  return <ExpoRoot context={ctx} />;
+}
+
+registerRootComponent(App);
